df9caf7e676a92c1aa574aebabf63eecc76ac209
jakob lambert-hartmann <jakob.lamberthartmann.canada@gmail.com>
2020-08-29 17:49:34 +0200

    upload.sh: create video

--- upload.sh
+++ upload.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-python3 ./gen.py
+python3 gen.py
 
-timidity ./testing.mid -Ow -o - \
-	| ffmpeg -loop 1 -i ./casseti.jpg -i - -c:a copy out.mp4
+timidity $1 -Ow -o wav.wav
+
+ffmpeg -y -loop 1 -framerate 1 -i cassetti.jpg -i wav.wav -c copy -shortest vid.mkv
+
+
+
