2c81433569be7ccd5e5622650b13a7239e1410c5
jakob lambert-hartmann <jakob.lamberthartmann.canada@gmail.com>
2020-09-06 10:28:38 +0200

    cleaned git root

--- create.sh
+++ create.sh
@@ -2,11 +2,11 @@
 
 dir="$(pwd)/$(dirname "$0")"
 
-$dir/gen.py $dir/data/mid.mid
+$dir/src/gen.py $dir/data/mid.mid
 
 timidity $dir/data/mid.mid -Ow -o $dir/data/wav.wav 1>/dev/null
 
-ffmpeg -y -loop 1 -framerate 1 -i $dir/cassetti.jpg -i $dir/data/wav.wav -c copy -shortest $dir/data/vid.mkv 1>/dev/null
+ffmpeg -y -loop 1 -framerate 1 -i $dir/img/cassetti.jpg -i $dir/data/wav.wav -c copy -shortest $dir/data/vid.mkv 1>/dev/null
 
 
 
--- create_upload.sh
+++ create_upload.sh
@@ -2,10 +2,10 @@
 
 dir="$(pwd)/$(dirname "$0")"
 
-$dir/gen.py $dir/data/mid.mid
+$dir/src/gen.py $dir/data/mid.mid
 
 timidity $dir/data/mid.mid -Ow -o $dir/data/wav.wav 1>/dev/null
 
-ffmpeg -y -loop 1 -framerate 1 -i $dir/cassetti.jpg -i $dir/data/wav.wav -c copy -shortest $dir/data/vid.mkv 1>/dev/null
+ffmpeg -y -loop 1 -framerate 1 -i $dir/img/cassetti.jpg -i $dir/data/wav.wav -c copy -shortest $dir/data/vid.mkv 1>/dev/null
 
-youtube-upload -t "$($dir/gen_title.py)" --privacy private --client-secrets secret.json $dir/data/vid.mkv
+youtube-upload -t "$($dir/src/gen_title.py)" --privacy private --client-secrets secret.json $dir/data/vid.mkv
